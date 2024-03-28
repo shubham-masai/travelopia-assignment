02187f8494bf053d7f5d3eeae8ff8e4938d4d523
Shubham Jayswal <shubhammasai03@gmail.com>
2024-03-28 18:10:33 +0530

    custom popup added

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 
-// const openPopupButton = document.getElementById('alertButton'); // Change to match HTML button ID
-// const popup = document.getElementById('popup');
-
-// openPopupButton.addEventListener('click', function () {
-//     popup.classList.remove('hidden-popup');
-// });
-
-
-const openPopupButton = document.getElementById('alertButton'); // Change to match HTML button ID
+const openPopupButton = document.getElementById('alertButton'); 
 const popup = document.getElementById('popup');
-const closePopupButton = popup.querySelector('.close-popup-button'); // Select the close button within the popup
+const closePopupButton = popup.querySelector('.close-popup-button');  
 
 openPopupButton.addEventListener('click', function () {
     popup.classList.remove('hidden-popup');
 });
 
 closePopupButton.addEventListener('click', function () {
-    popup.classList.add('hidden-popup'); // Hide the popup when close button is clicked
+    popup.classList.add('hidden-popup'); 
 });
