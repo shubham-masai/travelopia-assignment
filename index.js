3c72645a010f07c66b9199a0d6e310cc71b96174
Shubham Jayswal <shubhammasai03@gmail.com>
2024-03-28 18:08:42 +0530

    Added custom popup functionality.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 
-const button = document.getElementById('alertButton');
+// const openPopupButton = document.getElementById('alertButton'); // Change to match HTML button ID
+// const popup = document.getElementById('popup');
 
-button.addEventListener('click', function () {
-    alert("Hello, world!");
+// openPopupButton.addEventListener('click', function () {
+//     popup.classList.remove('hidden-popup');
+// });
+
+
+const openPopupButton = document.getElementById('alertButton'); // Change to match HTML button ID
+const popup = document.getElementById('popup');
+const closePopupButton = popup.querySelector('.close-popup-button'); // Select the close button within the popup
+
+openPopupButton.addEventListener('click', function () {
+    popup.classList.remove('hidden-popup');
+});
+
+closePopupButton.addEventListener('click', function () {
+    popup.classList.add('hidden-popup'); // Hide the popup when close button is clicked
 });
